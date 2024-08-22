eb79cb1aed676ad7a66634df94df163369ef209d
Jusaila <jusailap2000@gmail.com>
2024-08-22 20:30:30 +0530

    new

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
     public function cart(){
         return view('website.cart');
     }
+    public function create(Request $request){
+        $cart = Cart::create([
+            'user_id' => Auth::id(),
+            'product_id' => $request->product_id,
+            'quantity' => $request->quantity,
+        ]);
+    }
+
 }
