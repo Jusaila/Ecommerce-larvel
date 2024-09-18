ddda70cc9555dbbef872fefcdfee5b352b8437f7
Jusaila <jusailap2000@gmail.com>
2024-09-18 23:52:11 +0530

    add

--- app/Http/Controllers/CheckOutController.php
+++ app/Http/Controllers/CheckOutController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Address;
+use App\Models\Cart;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CheckOutController extends Controller
 {
     public function checkOut(){
-        return view('website.checkout');
+        $address = Address::where('status',1)->get();
+        $id = Auth::id();
+        $carts = Cart::with('product','user')->where('user_id', $id)->get();
+        $totalPrice = $carts->sum(function($cart) {
+            return $cart->product->price * $cart->quantity;
+        });
+        return view('website.checkout',['address' => $address ,'carts' => $carts, 'totalPrice' => $totalPrice]);
     }
 }
